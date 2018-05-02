b86ad856aa10210e02fbd94602b939bb4cb204f8
Raúl J. Capcha Anco <rauljcapcha@gmail.com>
2018-05-02 09:43:09 -0500

    Commit DIALOGFLOW

--- DSD_Bot/Program.cs
+++ DSD_Bot/Program.cs
@@ -13,13 +13,12 @@ namespace DSD_Bot
     {
         static void Main(string[] args)
         {
-
             preguntar();
-
         }
 
         static void preguntar()
         {
+            Console.WriteLine("Ingrese su pregunta acerca de REST");
             string pregunta = Console.ReadLine();
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:53637/ServicePreguntar.svc/Preguntar/" + pregunta + "");
             request.Method = "GET";
@@ -27,19 +26,12 @@ namespace DSD_Bot
             StreamReader reader = new StreamReader(response.GetResponseStream());
             string tramaJson = reader.ReadToEnd();
             JavaScriptSerializer js = new JavaScriptSerializer();
-
-
             Console.WriteLine(tramaJson);
-
-
-
             HttpWebRequest request2 = (HttpWebRequest)WebRequest.Create("http://localhost:53637/ServiceRespuestas.svc/Preguntar/" + tramaJson + "");
             request2.Method = "GET";
             HttpWebResponse response2 = (HttpWebResponse)request2.GetResponse();
             StreamReader reader2 = new StreamReader(response2.GetResponseStream());
-            
             preguntar();
-
         }
     }
 }
